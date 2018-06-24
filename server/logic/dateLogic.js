7d23cab8c44e657333ef047f6477b139ecc92dcc
Raphael Goeldner <goeldner@cip.ifi.lmu.de>
2018-06-25 00:00:00 +0200

    new route to add dates to an event, update readme

--- server/logic/dateLogic.js
+++ server/logic/dateLogic.js
@@ -1,6 +1,10 @@
 var mongodb = require('./../MongoDB/dbUtils');
 const participantLogic = require('./participantLogic');
+const eventLogic = require('./doodleEventLogic');
 var ResponseBuilder = require('./responseBuilder');
+var DateModel = require('./../models/date/doodleDateModel');
+const uuid = require('uuid/v4');
+var mongodb = require('./../MongoDB/dbUtils');
 
 /**
  * return all dates in the date collection in a callback
@@ -70,8 +74,99 @@ addDateToExistingParticipant = function (req, res, next) {
     });
 }
 
+/**
+ * calles by POST '/date/add/:creatorUUID'
+ * adds the dates in the request to the event in the event collection
+ * and creates new entries for them in the date collection
+ */
+addDatesToExistingEvent = function(req, res, next){
+    let responseBuilder = new ResponseBuilder();
+    let dates = req.body.dates;
+    generateDateIds(dates, newDateIds=>{
+        addDatesToDateCollection(req, res, next, newDateIds, responseBuilder).then(data =>{
+            addDatesToEvent(data.event, newDateIds, data.responseBuilder).then(responseBuilder =>{
+                responseBuilder.setMessage("Dates have been added to the event");
+                res.send(responseBuilder.getResponse());
+            }).catch(err =>{
+                responseBuilder.setMessage(responseBuilder.getDatabaseFailureMsg());
+                responseBuilder.setSuccess(false);
+                res.send(responseBuilder.getResponse());
+            });
+        });
+    });
+}
+
+/**
+ * generate one new id for each date in the dates array
+ * and return them in a callback
+ */
+generateDateIds = function(dates, callback){
+    let newDateIds = [];
+    dates.map(date =>{
+        newDateIds.push(uuid());
+    });
+    callback(newDateIds);
+}
+
+/**
+ * adds the dates from the request to the dates collection
+ * adds all the ids from the dateIds Array
+ */
+addDatesToDateCollection = function(req, res, next, dateIds, responseBuilder){
+    return new Promise((resolve, reject) =>{
+        let dates = req.body.dates;
+        let creatorUUID = req.params.creatorUUID;
+        console.log(dateIds);
+        getDoodleEventByCreatorUUID(creatorUUID, data=>{
+            let uuidEvent = data.uuidEvent;
+            let event = data.event;
+            // add dates to date collection
+            let i = 0;
+            dates.map(date =>{
+                let dateModel = new DateModel();
+                dateModel.setId(dateIds[i++], ()=>{
+                    dateModel.setUUID(uuidEvent, ()=>{
+                        dateModel.setModelProperty(date, ()=>{
+                            dateModel.saveModelInDatabase().then(data =>{
+                                responseBuilder.setSuccess(data.success);
+                            }).catch(err =>{
+                                console.log(err);
+                                reject(err);
+                            });
+                        });
+                    });
+                });
+            });
+            resolve({event: event, responseBuilder: responseBuilder});
+        }); 
+    });
+}
+
+/**
+ * updates the date array of an event in the event collection
+ * adds the date ids of the dateIds Array
+ */
+addDatesToEvent = function(event, dateIds, responseBuilder){
+    return new Promise((resolve, reject)=>{
+        let datesToUpdate = event.date;
+        dateIds.map(id =>{
+            datesToUpdate.push({date_id: id});
+        });
+        let criteria = {uuid: event.uuid};
+        let update = {date: datesToUpdate};
+        mongodb.updateItem(mongodb.doodleEventDBInfo.dbName, mongodb.doodleEventDBInfo.collectionName, criteria, update).then(data=>{
+            responseBuilder.setSuccess(data.success);
+            resolve(responseBuilder);
+        }).catch(err =>{
+            reject(err);
+        });
+    });
+ 
+}
+
 module.exports = {
     getAllDatesIntern: getAllDatesIntern,
     getDatesByEventId: getDatesByEventId,
-    addDateToExistingParticipant: addDateToExistingParticipant
+    addDateToExistingParticipant: addDateToExistingParticipant,
+    addDatesToExistingEvent: addDatesToExistingEvent
 }
--- server/routes/index.js
+++ server/routes/index.js
@@ -2,25 +2,65 @@ var express = require('express');
 var router = express.Router();
 var dbUtils = require('./../MongoDB/dbUtils');
 var logic = require('./../logic/doodleEventLogic.js');
+var dateLogic = require('./../logic/dateLogic');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-// create a new event
+/**
+ * create a new event
+ */
 router.post('/event/new', logic.saveNewDoodleEvent);
-// get an event by its uuid
+
+/**
+ * get an event by its uuid
+ */
 router.get('/event/:uuid', logic.sendEventDataToClient);
-// body: {name, email}, add one participant to an event with this uuid
+
+/**
+ * add one participant to an event with this uuid
+ */
 router.post('/participant/:uuid', logic.addParticipantToEvent);
-// add a participant to an existing date
+
+/**
+ * add a participant to an existing date
+ */
 router.post('/participant/add/date', logic.addDateToExistingParticipant);
 
 /**
  * update title, description, eventType, location of an event
  */
 router.post('/event/update/:creatorUUID', logic.updateDoodleEvent);
+
+/**
+ * add dates to an event if you are the creator
+ */
+router.post('/date/add/:creatorUUID', dateLogic.addDatesToExistingEvent);
+
+// TODO
+/**
+ * update dates of an event if you are the creator
+ */
+router.post('/date/update/:creatorUUID');
+
+/**
+ * delete dates of an event if you are the creator
+ */
+router.post('/date/delete/:creatorUUID');
+
+/**
+ * delete a participator of an event
+ */
+router.post('/participant/delete/:eventUUID');
+
+/**
+ * remove a date from a participant
+ */
+router.post('/participant/remove/date/:eventUUID');
+
+
 
 
 
