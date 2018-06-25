3d401fa832f164408a492d4a38ea4b5557fc0fa7
ri62vub <s.ghebremedhin@campus.lmu.de>
2018-06-25 11:59:05 +0200

    test

--- frontend/doodle-frontend/src/app/createEvent/03-calendar/calendar.component.ts
+++ frontend/doodle-frontend/src/app/createEvent/03-calendar/calendar.component.ts
@@ -1,9 +1,8 @@
 import {ChangeDetectionStrategy, Component, Inject, OnInit, TemplateRef} from '@angular/core';
-import {CalendarEvent, CalendarMonthViewDay} from 'angular-calendar';
+import { CalendarEvent } from 'angular-calendar';
 import * as moment from 'moment';
 import {CreateService} from "../../services/create.service";
 import {DatesModel} from "../../models/dates.model";
-import { DayViewHour } from 'calendar-utils';
 @Component({
   selector: 'app-calendar',
   templateUrl: './calendar.component.html',
@@ -12,12 +11,6 @@ import { DayViewHour } from 'calendar-utils';
 export class CalendarComponent implements OnInit {
   view: string = 'month';
   viewDate: Date = new Date();
-
-  selectedMonthViewDay: CalendarMonthViewDay;
-
-  selectedDayViewDate: Date;
-
-  dayView: DayViewHour[];
 
   events: CalendarEvent[] = [];
   createService: CreateService;
@@ -30,49 +23,15 @@ export class CalendarComponent implements OnInit {
   ngOnInit() {
   }
 
-  dayClicked(day: CalendarMonthViewDay): void {
-    if (this.selectedMonthViewDay) {
-      delete this.selectedMonthViewDay.cssClass;
-    }
-    day.cssClass = 'cal-day-selected';
-    this.selectedMonthViewDay = day;
+  dayClicked({ date, events }: { date: Date; events: CalendarEvent[] }): void {
+    const myTag = date.getDate();
+    const myMonat = moment.months(date.getMonth());
+    const myWochentag = moment.weekdaysShort(date.getDay());
+    const day = new DatesModel({tag: myTag, monat:myMonat, wochentag: myWochentag});
+    this.createService.addDay(day);
+
   }
 
-  beforeMonthViewRender({ body }: { body: CalendarMonthViewDay[] }): void {
-    body.forEach(day => {
-      if (
-        this.selectedMonthViewDay &&
-        day.date.getTime() === this.selectedMonthViewDay.date.getTime()
-      ) {
-        day.cssClass = 'cal-day-selected';
-        this.selectedMonthViewDay = day;
-      }
-    });
-  }
-
-  hourSegmentClicked(date: Date) {
-    this.selectedDayViewDate = date;
-    this.addSelectedDayViewClass();
-  }
-
-  beforeDayViewRender(dayView: DayViewHour[]) {
-    this.dayView = dayView;
-    this.addSelectedDayViewClass();
-  }
-
-  private addSelectedDayViewClass() {
-    this.dayView.forEach(hourSegment => {
-      hourSegment.segments.forEach(segment => {
-        delete segment.cssClass;
-        if (
-          this.selectedDayViewDate &&
-          segment.date.getTime() === this.selectedDayViewDate.getTime()
-        ) {
-          segment.cssClass = 'cal-day-selected';
-        }
-      });
-    });
-  }
 
 
 
--- frontend/doodle-frontend/src/app/models/creator.model.ts
+++ frontend/doodle-frontend/src/app/models/creator.model.ts
@@ -1,8 +1,12 @@
 export class CreatorModel{
   name: string;
   email: string;
+  title: string;
+  description: string;
   constructor() {
-    this.name = 'Name';
-    this.email = 'E-Mail';
+    this.title = '';
+    this.description = '';
+    this.name = '';
+    this.email = '';
   }
 }
