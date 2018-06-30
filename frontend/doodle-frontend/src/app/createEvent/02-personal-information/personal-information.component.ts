5cd990eee85c1adbc0d571b4b388edd204002b9b
anikint <antiengo@gmail.com>
2018-06-30 23:35:58 +0200

    newstuff

--- frontend/doodle-frontend/src/app/createEvent/02-personal-information/personal-information.component.ts
+++ frontend/doodle-frontend/src/app/createEvent/02-personal-information/personal-information.component.ts
@@ -1,6 +1,4 @@
 import {Component, Inject, OnChanges, OnInit} from '@angular/core';
-import {CreateService} from "../../services/create.service";
-import {DateService} from "../../services/date.service";
 
 @Component({
   selector: 'app-personal-information',
@@ -8,14 +6,9 @@ import {DateService} from "../../services/date.service";
   styleUrls: ['./personal-information.component.css']
 })
 export class PersonalInformationComponent implements OnInit, OnChanges {
-  mail:string;
-  createService: CreateService;
-  constructor(@Inject(CreateService) createService: CreateService){
-    this.createService = createService;
-  }
 
   ngOnChanges(){
-    console.log(this.createService.userInformation.name);
+
   }
   ngOnInit() {
   }
