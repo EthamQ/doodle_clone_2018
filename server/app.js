4754fdeceb9ac091554eef4ef944c1aaf46eba2c
ri62vub <s.ghebremedhin@campus.lmu.de>
2018-07-02 00:11:16 +0200

    jap

--- server/app.js
+++ server/app.js
@@ -17,7 +17,7 @@ app.set('view engine', 'jade');
 app.use(function (req, res, next) {
 
   // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+  res.setHeader('Access-Control-Allow-Origin', 'http://10.150.133.137:4200');
 
   // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
