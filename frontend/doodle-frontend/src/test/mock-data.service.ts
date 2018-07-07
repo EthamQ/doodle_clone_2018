55786efc545dd9c93d0713419c07dedd1d4e7559
anikint <antiengo@gmail.com>
2018-07-07 19:15:30 +0200

    error message if wrong id, bei view funzt es nicht da serverstruktur

---
+++ frontend/doodle-frontend/src/test/mock-data.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MockDataService {
+
+  constructor() { }
+
+  createInput =
+    {
+      title: 'thai\'s promi dinner',
+      location: 'thai\'s living room',
+      'description': 'my mom is not at home',
+      'creator': {
+        'name': 'thai',
+      },
+      'date': [
+        {
+          'date': 'Fri, Jul 6, 2018 12:00 AM',
+          'timeFrom': 1530828000,
+          'timeTo': 1530828000
+        },
+        {
+          'date': 'Thu, Jul 5, 2018 3:45 AM',
+          'timeFrom': 1530755100,
+          'timeTo': 1530820800
+        }
+      ]
+    };
+
+
+
+}
